ef74c23a6f2beaeab54f6a06726bbef6ec36eaf3
louisdev12 <221082834+louisdev12@users.noreply.github.com>
2025-08-15 00:33:17 +0300

    feat: changed version number

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read README.md with UTF-8 encoding
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="toast_animation",
-    version="0.1",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         'flet',
@@ -10,7 +15,7 @@ setup(
     author="Heckerdev",
     author_email="simplelogin-newsletter.unwilling149@simplelogin.com",
     description="A package for creating animated toast notifications in Flet applications.",
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/louisdev12/flet_toasty.git",
     classifiers=[
